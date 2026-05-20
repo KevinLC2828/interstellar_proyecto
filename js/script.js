20c912a4705735d5379cf9b762d906470ae9e2b4
Kevin Lozada <kevinlozada028@gmai.com>
2026-05-20 17:18:44 -0500

    Agrego cards de personajes

--- js/script.js
+++ js/script.js
@@ -81,3 +81,33 @@ if(menuToggle){
     });
 
 }
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach((slide) => {
+
+        slide.classList.remove("active");
+
+    });
+
+    slides[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+}
+
+setInterval(nextSlide, 3000);
